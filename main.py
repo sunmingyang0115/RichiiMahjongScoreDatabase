bd15d4a44121c81857b83261953a0dfdaf062a5f
Eric Sun <87628485+sunmingyang0115@users.noreply.github.com>
2023-11-19 17:00:25 -0500

    upload main.py
    real

---
+++ main.py
@@ -0,0 +1,23 @@
+import discord
+import csv
+
+token = 'aminger'
+with open('token.txt') as file:
+    token = file.read()
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as', self.user)
+
+    async def on_message(self, message):
+        # don't respond to ourselves
+        if message.author == self.user:
+            return
+
+        if message.content == 'ping':
+            await message.channel.send('pong')
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = MyClient(intents=intents)
+client.run(token)
