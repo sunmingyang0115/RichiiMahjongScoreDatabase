c51b7c837034ec8d161ccae1c3749cceddcd93b3
psun256 <paulsun700@gmail.com>
2024-02-06 18:49:43 -0500

    tests

---
+++ module/tests/test_mjgame.py
@@ -0,0 +1,21 @@
+import unittest
+from mjgame import MJGameNya
+
+class TestMJGameInputNya(unittest.TestCase):
+    def test_regular_game(self):
+        pass
+    def test_mixed_repr(self):
+        pass
+    def test_invalid_scores(self):
+        pass
+    def test_invalid_mention(self):
+        pass
+    def test_leading_ws(self):
+        pass
+    def test_trailing_ws(self):
+        pass
+    def test_radiohead(self):
+        pass
+
+class TestMJGameOtherStuffNya(unittest.TestCase):
+    pass
