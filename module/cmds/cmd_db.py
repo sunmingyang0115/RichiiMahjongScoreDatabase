29f6f88e2867b0dbce76c41dd97e9f7971d0a640
Eric Sun <87628485+sunmingyang0115@users.noreply.github.com>
2024-02-09 14:25:24 -0500

    Update cmd_db.py

--- module/cmds/cmd_db.py
+++ module/cmds/cmd_db.py
@@ -1,51 +1,27 @@
-from __future__ import annotations
+# from __future__ import annotations
 
-from datetime import date
-import discord
-from db import GameRecordNya
-from util import ping_to_userid
+# from datetime import date
+# import discord
+# from db import GameRecordNya
+# from util import ping_to_userid
 
-from typing import TYPE_CHECKING
-if TYPE_CHECKING:
-    from typing import Union
-    from bot import BotClient
-
-def find_score2(frags: list[str]) -> Union[list[tuple[str, int]], None]:
-    tot = 0
-    score_list = []
-    for i in range(3, 11, 2):
-        tot += float(frags[i + 1])
-    if 90 <= tot <= 100 and abs(round(tot) - tot) < 1e-6:
-        for i in range(3, 11, 2):
-            usr_id = ping_to_userid(frags[i])
-            score = float(frags[i + 1])
-            score_list.append((usr_id, 100 * round(10 * score)))
-        return score_list
-    elif tot % 1000 == 0:
-        for i in range(3, 11, 2):
-            usr_id = ping_to_userid(frags[i])
-            score = int(frags[i + 1])
-            score_list.append((usr_id, 100 * round(10 * score)))
-        return score_list
-    else:
-        return None
+# from typing import TYPE_CHECKING
+# if TYPE_CHECKING:
+#     from typing import Union
+#     from bot import BotClient
 
 
-async def on_cmd_db_store(self: BotClient, message: discord.Message, frags: list[str]):
-    players = find_score2(frags)
-    if players is None:
-        raise RuntimeError("Invalid Data")
-    record = GameRecordNya("discord:" + message.id, str(date.today()), [v[0] for v in players], [v[1] for v in players])
-    self.db.new_game(record)
-    await message.add_reaction('✅')
+
+# # async def on_cmd_db_store(self: BotClient, message: discord.Message, frags: list[str]):
+# #     # ron db store
 
 
-async def on_cmd_db_get(self: BotClient, message: discord.Message, frags: list[str]):
-    # frags[3] is user id
-    if len(frags) != 4:
-        raise RuntimeError("Not enough/too many arguments!")
-    usr = self.db.get_user_games(ping_to_userid(frags[3]))
-    out = ''
-    for e in usr:
-        out = out + str(e) + "\n"
-    await message.channel.send(out)
+# async def on_cmd_db_get(self: BotClient, message: discord.Message, frags: list[str]):
+#     # frags[3] is user id
+#     if len(frags) != 4:
+#         raise RuntimeError("Not enough/too many arguments!")
+#     usr = self.db.get_user_games(ping_to_userid(frags[3]))
+#     out = ''
+#     for e in usr:
+#         out = out + str(e) + "\n"
+#     await message.channel.send(out)
